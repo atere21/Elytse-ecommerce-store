59bdd8c8e1d1fc3a0e9290d41240e73938533b97
atere21 <tosinateret@gmail.com>
2023-04-04 10:49:41 -0600

    my ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet
+}
+  from "react-router-dom";
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = () =>{
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+     {
+      path:"/",
+      element:<Home/>,
+     },
+
+     {
+      path:"/products/:id",
+      element:<Products/>,
+     },
+
+     {
+      path:"/product/:id",
+      element:<Product/>,
+     },
+    ],
+  }
+    ]);
+  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}/>
     </div>
   );
 }
