776c1146668774c436bdc7dfa3526dc6ab8a8f2c
atere21 <tosinateret@gmail.com>
2023-04-10 13:05:54 -0600

    elytse

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
-import './App.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet
-}
-  from "react-router-dom";
-import Home from './pages/Home/Home';
-import Product from './pages/Product/Product';
-import Products from './pages/Products/Products';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import "./app.scss"
+import React from 'react'
 
-const Layout = () =>{
+const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
-    </div>
+    <div>App</div>
   )
 }
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Layout/>,
-    children:[
-     {
-      path:"/",
-      element:<Home/>,
-     },
 
-     {
-      path:"/Products/:id",
-      element:<Products/>,
-     },
-
-     {
-      path:"/Product/:id",
-      element:<Product/>,
-     },
-    ],
-  }
-    ]);
-  
-
-function App() {
-  return (
-    <div>
-      <RouterProvider router={router}/>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/pages/Product/Product.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Product = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default Product
